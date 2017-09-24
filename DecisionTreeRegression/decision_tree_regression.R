b4d8a44829ff33825e9f6f33ebe351f75931eb89
Deepit Patel <detel@users.noreply.github.com>
2017-09-24 23:45:54 +0530

    Create decision_tree_regression.R

---
+++ DecisionTreeRegression/decision_tree_regression.R
@@ -0,0 +1,29 @@
+# Importing the dataset
+dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[2:3]
+
+# install.packages('rpart')
+library(rpart)
+regressor = rpart(formula = Salary ~ .,
+                  data = dataset,
+                  control = rpart.control(minsplit = 1))
+
+# Predicting a new result with Decision Tree Regression
+y_pred = predict(regressor, data.frame(Level = 6.5))
+
+# Visualising the Decision Tree Regression results (higher resolution)
+# install.packages('ggplot2')
+library(ggplot2)
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.01)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') +
+  geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
+            colour = 'blue') +
+  ggtitle('Truth or Bluff (Decision Tree Regression)') +
+  xlab('Level') +
+  ylab('Salary')
+
+# Plotting the tree
+plot(regressor)
+text(regressor)
